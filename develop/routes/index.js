94fc9c7cec73cb90542f002cb50592e6b81de52f
slaughtercasandra787 <slaughtercasandra787@gmail.com>
2024-07-08 18:28:58 -0500

    fixed files

--- develop/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router =reqire('express').Router();
-const commentsRoutes= require('../controllers/comments.js');
-const blogRoutes= require('../controllers/blogs.js');
-const postRoutes =require('../controllers/users.js');
-
-  module.exports =Rroutes;
